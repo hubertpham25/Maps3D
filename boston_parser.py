051ba49ce9dfef6c25d4494d2a28fb82ae94870d
hubertpham25 <phamhubert820@gmail.com>
2025-08-08 23:00:04 -0400

    oh boston

---
+++ boston_parser.py
@@ -0,0 +1,44 @@
+import xml.etree.ElementTree as ET
+
+def tree_parser():
+    tree = ET.parse('boston_map.osm')
+    root = tree.getroot()
+
+    nodes = dict() #nodes
+    ways = dict() #edges
+
+    #starts at root node and finds everything that has 'node'
+    for node in root.findall('node'):
+        #grab node id, find coords, find node tags
+        node_id = int(node.get('id'))
+        lat = float(node.get('lat'))
+        lon = float(node.get('lon'))
+        nd_tags = {tag.get('k'): tag.get('v') for tag in node.findall('tag')}
+        coord = (lat, lon)
+
+        nodes[node_id] = {
+            'coord': coord,
+            'tags': nd_tags,
+            'ways': set()
+        }
+
+        #starts at root node and finds everything that has 'node'
+        for way in root.findall('way'):
+            # loop should get the way id, 
+            # node references (nodes that share ways), 
+            # way tags. different from node tags
+            way_id = int(way.get('id'))
+            nd_ref = [int(nd.get('ref') for nd in way.findall('nd'))]
+            way_tags = {tag.get('k'): tag.get('v') for tag in way.findall('tag')}
+            ways[way_id] = {'nodes': nd_ref,
+                            'tags': way_tags}
+            
+            #loop should go through reference nodes in nd_ref (list), 
+            # check if ref is a key in nodes, 
+            # if yes, add the way id to 'ways'
+            for ref in nd_ref:
+                if ref in nodes:
+                    nodes[ref]['ways'].add(way_id)
+            
+
+        
